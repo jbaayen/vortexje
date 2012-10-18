7a41210e553355da27912e73e80120338bbf2d45
Jorn Baayen <jorn.baayen@baayen-heinz.com>
2012-10-18 20:19:55 +0200

    Fix build.

--- test/test-wing-builder.cpp
+++ test/test-wing-builder.cpp
@@ -59,7 +59,7 @@ main (int argc, char **argv)
     
     // Prescribe angle of attack:
     double alpha = 5.0 / 180.0 * M_PI;
-    wing.rotate(span_direction, -alpha);
+    wing.rotate(wing.span_direction, -alpha);
     
     // Create mesh collection:
     Collection collection(string("test-wing"),
