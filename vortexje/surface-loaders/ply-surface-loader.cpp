5ea746e23768859d58020989fa65bb55ce9c3ac5
Jorn Baayen <jorn.baayen@baayen-heinz.com>
2014-01-29 12:29:27 -0500

    Add PLY loader.

---
+++ vortexje/surface-loaders/ply-surface-loader.cpp
@@ -0,0 +1,145 @@
+//
+// Vortexje -- PLY surface loader.
+//
+// Copyright (C) 2014 Baayen & Heinz GmbH.
+//
+// Authors: Jorn Baayen <jorn.baayen@baayen-heinz.com>
+//
+
+#include <iostream>
+
+#include <vortexje/surface-loaders/ply-surface-loader.hpp>
+
+#include "rply/rply.h"
+
+using namespace std;
+using namespace Eigen;
+using namespace Vortexje;
+using namespace Vortexje::SurfaceLoaders;
+
+/**
+   Returns the PLY file extension (".msh").
+   
+   @returns The PLY file extension (".msh").
+*/
+const char *
+PLYSurfaceLoader::file_extension() const
+{
+    return ".ply";
+}
+
+// rply parser callbacks.
+static int vertex_cb(p_ply_argument argument)
+{
+    void *ptr;
+    long index;
+    ply_get_argument_user_data(argument, &ptr, &index);
+    
+    PLYSurfaceLoader *loader = (PLYSurfaceLoader *) ptr;
+    
+    loader->read_vertex_coordinate(index, ply_get_argument_value(argument));
+    
+    return 1;
+}
+
+static int face_cb(p_ply_argument argument)
+{  
+    void *ptr;
+    ply_get_argument_user_data(argument, &ptr, NULL);
+    
+    PLYSurfaceLoader *loader = (PLYSurfaceLoader *) ptr;
+    
+    long length, index;
+    ply_get_argument_property(argument, NULL, &length, &index);
+    
+    if (index >= 0)
+        loader->read_panel_node(index, length, (int) ply_get_argument_value(argument));
+    
+    return 1;
+}
+
+/**
+   Loads and parses the contents of a PLY file into a Surface.
+
+   @param[in]   surface    Surface to load to.
+   @param[in]   filename   Filename pointing to the PLY file to load.
+   
+   @returns true on success.
+*/
+bool
+PLYSurfaceLoader::load(Surface &surface, const string &filename)
+{
+    cout << "Surface " << surface.id << ": Loading from " << filename << "." << endl;
+    
+    this->surface = &surface;
+    
+    current_panel = 0;
+    
+    // Load surface from PLY file.
+    // We use "rply" from http://w3.impa.br/~diego/software/rply/, available under the MIT license.
+    p_ply ply = ply_open(filename.c_str(), NULL, 0, NULL); 
+    if (!ply)
+        return false;
+        
+    if (!ply_read_header(ply))
+        return false;
+        
+    ply_set_read_cb(ply, "vertex", "x", vertex_cb, this, 0);
+    ply_set_read_cb(ply, "vertex", "y", vertex_cb, this, 1);
+    ply_set_read_cb(ply, "vertex", "z", vertex_cb, this, 2);
+    
+    ply_set_read_cb(ply, "face", "vertex_indices", face_cb, this, 0);
+    
+    if (!ply_read(ply))
+        return false;
+        
+    ply_close(ply);
+    
+    // Compute surface topology:
+    surface.compute_topology();
+    
+    // Compute panel geometry:
+    surface.compute_geometry();
+    
+    // Done:
+    return true;
+}
+
+/**
+   Internal interface method to rply library.
+*/
+void
+PLYSurfaceLoader::read_vertex_coordinate(int index, double value)
+{
+    current_point(index) = value;
+    
+    if (index == 2) {
+        // This is the last coordinate.  Process vertex.
+        surface->nodes.push_back(current_point);      
+            
+        vector<int> *neighbor_list = new vector<int>;
+        surface->node_panel_neighbors.push_back(neighbor_list);
+        
+        surface->node_deformation_velocities.push_back(Vector3d(0, 0, 0));
+    }
+}
+
+/**
+   Internal interface method to rply library.
+*/
+void
+PLYSurfaceLoader::read_panel_node(int index, int length, int node)
+{
+    current_panel_nodes.push_back(node);
+    
+    surface->node_panel_neighbors[node]->push_back(current_panel);
+    
+    if (index == length - 1) {
+        // This is the last node.  Process panel.
+        surface->panel_nodes.push_back(current_panel_nodes);
+        
+        current_panel_nodes.clear();
+        
+        current_panel++;
+    }
+}
---
+++ vortexje/surface-loaders/ply-surface-loader.hpp
@@ -0,0 +1,52 @@
+//
+// Vortexje -- PLY surface loader.
+//
+// Copyright (C) 2014 Baayen & Heinz GmbH.
+//
+// Authors: Jorn Baayen <jorn.baayen@baayen-heinz.com>
+//
+
+#ifndef __PLY_SURFACE_LOADER_HPP__
+#define __PLY_SURFACE_LOADER_HPP__
+
+#include <string>
+
+#include <vortexje/surface-loader.hpp>
+
+namespace Vortexje
+{
+
+namespace SurfaceLoaders
+{
+
+/**
+   Polygon File Format/Stanford Triangle Format loader.
+   
+   @brief PLY surface loader.
+*/
+class PLYSurfaceLoader : public SurfaceLoader
+{
+public:
+    const char *file_extension() const;
+    
+    bool load(Surface &surface, const std::string &filename);
+    
+    // rply callbacks.
+    void read_vertex_coordinate(int index, double value);
+    void read_panel_node(int index, int length, int node);
+    
+private:
+    // This class is not re-entrant.
+    Surface *surface;
+    
+    int current_panel;
+    
+    Eigen::Vector3d current_point;
+    std::vector<int> current_panel_nodes;  
+};
+
+};
+
+};
+
+#endif // __PLY_SURFACE_LOADER_HPP__
