44b23607db94ad45c478c26c637b53c8e9670a35
Jorn Baayen <jorn.baayen@baayen-heinz.com>
2014-02-05 11:05:58 -0500

    Add oscillating wing section example.

--- examples/naca0012-section-oscillation.cpp
+++ examples/naca0012-section-oscillation.cpp
@@ -1,19 +1,18 @@
 //
-// Vortexje -- Clark-Y wing section construction example.
+// Vortexje -- NACA0012 airfoil section in harmonic oscillation.
 //
-// Copyright (C) 2012 - 2014 Baayen & Heinz GmbH.
+// Copyright (C) 2014 Baayen & Heinz GmbH.
 //
 // Authors: Jorn Baayen <jorn.baayen@baayen-heinz.com>
 //
 
 #include <iostream>
+#include <fstream>
 
 #include <vortexje/solver.hpp>
-#include <vortexje/parameters.hpp>
 #include <vortexje/lifting-surface-builder.hpp>
-#include <vortexje/shape-generators/airfoils/clark-y-airfoil-generator.hpp>
+#include <vortexje/shape-generators/airfoils/naca4-airfoil-generator.hpp>
 #include <vortexje/surface-writers/vtk-surface-writer.hpp>
-#include <vortexje/field-writers/vtk-field-writer.hpp>
 
 using namespace std;
 using namespace Eigen;
@@ -22,20 +21,20 @@ using namespace Vortexje;
 int
 main (int argc, char **argv)
 {
-    // Enable wake convection:
-    Parameters::convect_wake = true;
+    // Set up parameters for unsteady simulation:
+    Parameters::unsteady_bernoulli = true;
+    Parameters::convect_wake       = true;
     
-    // Create lifting surface object:
+    // Create wing:
     LiftingSurface wing;
-
-    // Construct wing section:
+    
     LiftingSurfaceBuilder surface_builder(wing);
 
+    const double chord = 0.75;
+    const double span = 4.5;
+    
     const int n_points_per_airfoil = 32;
     const int n_airfoils = 21;
-    
-    const double chord = 1.0;
-    const double span = 5.0;
     
     int trailing_edge_point_id;
     vector<int> prev_airfoil_nodes;
@@ -45,7 +44,7 @@ main (int argc, char **argv)
     
     for (int i = 0; i < n_airfoils; i++) {
         vector<Vector3d, Eigen::aligned_allocator<Vector3d> > airfoil_points =
-            ClarkYAirfoilGenerator::generate(chord, n_points_per_airfoil, trailing_edge_point_id);
+            NACA4AirfoilGenerator::generate(0, 0, 0.12, true, chord, n_points_per_airfoil, trailing_edge_point_id);
         for (int j = 0; j < (int) airfoil_points.size(); j++)
             airfoil_points[j](2) += i * span / (double) (n_airfoils - 1);
              
@@ -62,20 +61,17 @@ main (int argc, char **argv)
 
     surface_builder.finish(node_strips, panel_strips, trailing_edge_point_id);
     
-    // Translate into the canonical coordinate system:
-    Vector3d translation(-chord / 3.0, 0.0, -span / 2.0);
-    wing.translate(translation);
-    
-    // Prescribe angle of attack:
-    double alpha = 5.0 / 180.0 * M_PI;
-    wing.rotate(Vector3d::UnitZ(), -alpha);
-    
-    // Create surface body:
+    // Create body:
     Body body(string("section"));
     body.add_lifting_surface(&wing);
     
+    // Set up oscillation:
+    double alpha_max = 10.0 / 180.0 * M_PI;
+    
+    double omega = 2 * M_PI / 1.0;
+    
     // Set up solver:
-    Solver solver("clark-y-section-log");
+    Solver solver("naca0012-section-oscillation-log");
     solver.add_body(body);
     
     Vector3d freestream_velocity(30, 0, 0);
@@ -84,36 +80,55 @@ main (int argc, char **argv)
     double fluid_density = 1.2;
     solver.set_fluid_density(fluid_density);
     
-    // Set up surface writer:
-    VTKSurfaceWriter surface_writer;
-    
-    // Set up field writer:
-    VTKFieldWriter field_writer;
-    
-    // Run simulation:
-    double t = 0.0;
     double dt = 0.01;
-    int step_number = 0;
     
+    // Set up logging:
+    VTKSurfaceWriter writer;
+    
+    ofstream f;
+    f.open("naca0012-section-oscillation-log/forces.txt");
+    
+    // Compute:
+    double alpha = 0.0;
+    double t = 0.0;
+    int i = 0;
+
     solver.initialize_wakes(dt);
-    while (t < 60) {
+
+    while (t < 30.0) {
         // Solve:
         solver.solve(dt);
         
-        // Log coefficients:
-        solver.log(step_number, surface_writer);
+        // Log source, doublet, and pressure distributions:
+        solver.log(i, writer);
         
-        // Enable below to log the velocity field:
-        // field_writer.write_velocity_field(solver, "velocity-field.vtk", 0.1, 0.1, 0.1, 0.2, 0.2, 0.2);
+        // Log lift and drag coefficients:
+        Vector3d F_a = solver.force(body);
+
+        double q = 0.5 * fluid_density * freestream_velocity.squaredNorm();
+
+        double S = chord * span;
+
+        double C_L = F_a(1) / (q * S);
+        double C_D = F_a(0) / (q * S);
+
+        f << alpha << ' ' << C_L << ' ' << C_D << endl;
         
         // Update wake:
         solver.update_wakes(dt);
         
+        // Rotate wing:
+        alpha = alpha_max * sin(omega * t);
+        Quaterniond attitude = AngleAxis<double>(alpha, Vector3d::UnitZ()) * Quaterniond(1, 0, 0, 0);
+        body.set_attitude(attitude);
+        
         // Step time:
         t += dt;
-        step_number++;
-    }
+        i++;
+    }  
     
-    // Done:
+    f.close();
+
+    // Done.
     return 0;
 }
