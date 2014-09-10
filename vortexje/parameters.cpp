d35371992a87d0837045483d87fc68f1a2991002
Jorn Baayen <jorn.baayen@baayen-heinz.com>
2014-09-10 14:38:03 +0200

    Default tolerances to machine epsilon.

--- vortexje/parameters.cpp
+++ vortexje/parameters.cpp
@@ -7,15 +7,17 @@
 //
 
 #include <cmath>
+#include <limits>
 
 #include <vortexje/parameters.hpp>
 
 using namespace Vortexje;
+using namespace std;
 
 // Default values:
 int    Parameters::linear_solver_max_iterations       = 20000;
 
-double Parameters::linear_solver_tolerance            = 1e-10;
+double Parameters::linear_solver_tolerance            = numeric_limits<double>::epsilon();
 
 bool   Parameters::unsteady_bernoulli                 = true;
 
@@ -27,12 +29,12 @@ double Parameters::wake_emission_distance_factor      = 0.25;
 
 double Parameters::static_wake_length                 = 100.0;
 
-double Parameters::inversion_tolerance                = 1e-7;
+double Parameters::inversion_tolerance                = numeric_limits<double>::epsilon();
 
-double Parameters::collocation_point_delta            = 1e-7;
+double Parameters::collocation_point_delta            = 1e-12;
 
 bool   Parameters::marcov_surface_velocity            = false;
 
 int    Parameters::max_boundary_layer_iterations      = 100;
 
-double Parameters::boundary_layer_iteration_tolerance = 1e-5;
+double Parameters::boundary_layer_iteration_tolerance = numeric_limits<double>::epsilon();
